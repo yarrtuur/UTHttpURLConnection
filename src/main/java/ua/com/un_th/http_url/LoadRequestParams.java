d6bd5bb97ae576f1e88689ef4af2032c41936000
yarrtuur <sashagmaster@gmail.com>
2018-07-21 23:08:56 +0300

    get file data todo

--- src/main/java/ua/com/un_th/http_url/LoadRequestParams.java
+++ src/main/java/ua/com/un_th/http_url/LoadRequestParams.java
@@ -5,26 +5,41 @@ import com.ebay.xcelite.reader.SheetReader;
 import com.ebay.xcelite.sheet.XceliteSheet;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Collection;
 
 public class LoadRequestParams {
 	private ConnectionProperties connectionProperties;
 	private Collection<DataNode> requestList;
+	private static final String CONFIG_REQUEST_DATA = "requestFile.xlsx";
 
 	public LoadRequestParams() throws ExitException {
 		connectionProperties = new ConnectionProperties();
 		getRequestData();
 	}
 
-	public Collection<DataNode> getRequestList() {
-		return requestList;
-	}
+	private void getRequestData() throws ExitException {
+		URL url;
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		try (
+				InputStream in = classloader.getResourceAsStream(CONFIG_REQUEST_DATA)
 
-	private void getRequestData() {
-		Xcelite xcelite = new Xcelite(new File("requestFile.xlsx"));
+		) {
+			url = classloader.getResource("requestFile.xlsx");
+			System.out.println(url);
+		} catch (IOException | NullPointerException e) {
+			throw new ExitException("No file .properties found in " + e.getMessage());
+		}
+		Xcelite xcelite = new Xcelite(new File(String.valueOf(url)));
 		XceliteSheet sheet = xcelite.getSheet("request");
 		SheetReader<DataNode> reader = sheet.getBeanReader(DataNode.class);
 		requestList = reader.read();
+	}
+
+	public Collection<DataNode> getRequestList() {
+		return requestList;
 	}
 
 	public String getUrlConnect() {
