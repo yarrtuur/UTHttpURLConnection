a960003c2aa1d725dfae4cbd715315e19841aae4
yarrtuur <sashagmaster@gmail.com>
2018-07-26 06:13:09 +0300

    RequestMaker todo

--- src/main/java/ua/com/un_th/http_url/Main.java
+++ src/main/java/ua/com/un_th/http_url/Main.java
@@ -2,6 +2,7 @@ package ua.com.un_th.http_url;
 
 import ua.com.un_th.http_url.data_containers.RequestNode;
 import ua.com.un_th.http_url.utils.RequestDataReader;
+import ua.com.un_th.http_url.utils.RequestHolder;
 
 import java.util.Collection;
 
@@ -12,7 +13,7 @@ public class Main {
 
             RequestDataReader requestDataReader = new RequestDataReader( "requestFile.xlsx");
             Collection<RequestNode> collectionRequests = requestDataReader.getRequestData();
-
+            RequestHolder requestHolder = new RequestHolder(collectionRequests);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
--- src/main/java/ua/com/un_th/http_url/utils/RequestHolder.java
+++ src/main/java/ua/com/un_th/http_url/utils/RequestHolder.java
@@ -8,19 +8,19 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
-public class RequestMaker {
+public class RequestHolder {
     String userNameAndPasswd;
     private LoadRequestParams loader;
     private String postUrl;
     private HttpUrlRequest httpUrlRequest;
 
-    public RequestMaker(Collection<RequestNode> requestMap) throws ExitException {
+    public RequestHolder(Collection<RequestNode> requestMap) throws ExitException {
         this.loader = new LoadRequestParams();
         this.postUrl = loader.getUrlConnect();
         this.userNameAndPasswd = loader.getUserNameAndPasswd();
     }
 
-    public Map<String, String>  sendRequest()  {
+    public Map<String, String>  sendRequest(Map<String, String> condition)  {
         return Collections.emptyMap();
     }
 
